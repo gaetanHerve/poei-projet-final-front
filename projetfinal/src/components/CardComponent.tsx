d9daefa4e99e8347ac20bc9e85b444a9e43d470f
Laure Le Nouail <laure.lenouail@gmail.com>
2023-05-16 16:32:28 +0200

    page accueil fini

---
+++ projetfinal/src/components/CardComponent.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import ButtonComponent from "./ButtonComponent";
+import "./Card.css";
+
+function CardComponent({ animalList }) {
+  const MAX_LENGTH = 70;
+  return (
+    <div className="card-principal">
+      {animalList.length > 0 &&
+        animalList.map((animal, index) => (
+          <div key={index} className="carte">
+            <div className="shadow carte-principal">
+              <img
+                src={`assets/${animal.urlImage}`}
+                className="card-image-animal"
+              />
+              <div className="card-container-text">
+                <h4 className="card-nom">{animal.nom}</h4>
+                <div className="card-information">
+                  <div className="card-information-gender">
+                    <img
+                      className="card-information-logo"
+                      src="assets/gender.png"
+                    />
+                    <p className="card-information-text">{animal.sexe}</p>
+                  </div>
+
+                  <div className="card-information-age">
+                    <img
+                      className="card-information-logo"
+                      src="assets/calendar.png"
+                    />
+                    <p className="card-information-text">{animal.age} ans</p>
+                  </div>
+                </div>
+                <p className="card-text-description">{`${animal.description.substring(
+                  0,
+                  MAX_LENGTH
+                )}...`}</p>
+                <div className="card-button">
+                  <ButtonComponent
+                    lien={`/findbyid/${animal.id}`}
+                    text="Voir sa fiche"
+                    handleOnClick={() => {}}
+                  ></ButtonComponent>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+    </div>
+  );
+}
+
+export default CardComponent;
