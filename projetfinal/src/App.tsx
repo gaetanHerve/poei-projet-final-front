6331cea751efaab9651034a5f0fa17bb38dc4f0e
Anton-mnc <antoine.mgy.mnc@gmail.com>
2023-05-16 17:25:05 +0200

    update et harmonisation

--- projetfinal/src/App.tsx
+++ projetfinal/src/App.tsx
@@ -1,27 +1,27 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import PageTest from './components/PageTest';
-import NavBar from './components/NavBar';
-import TestCarousel from './components/TestCarousel';
-import TestNavbarFooter from './components/TestNavbarFooter';
-import Footer from './components/Footer';
+import PageTest from "./components/PageTest";
+import NavBar from "./components/NavBar";
+import TestNavbarFooter from "./components/TestNavbarFooter";
+import Footer from "./components/Footer";
+import PageAccueil from "./components/PageAccueil";
 
 function App() {
-  return (
-    <div className="App">
-      <NavBar></NavBar>
-      <BrowserRouter>
-        <Routes>
-          <Route path="pagetest" element={<PageTest/>} />
+	return (
+		<div className="App">
+			<NavBar></NavBar>
+			<BrowserRouter>
+				<Routes>
+					<Route path="pagetest" element={<PageTest />} />
 
-          <Route path="/" element={<PageAccueil/>} />
-          <Route path="testfooter" element={<TestNavbarFooter/>} />
-        </Routes>
-      </BrowserRouter>
-      <Footer></Footer>
-    </div>
-  );
+					<Route path="/" element={<PageAccueil />} />
+					<Route path="testfooter" element={<TestNavbarFooter />} />
+				</Routes>
+			</BrowserRouter>
+			<Footer></Footer>
+		</div>
+	);
 }
 
 export default App;
--- projetfinal/src/components/Footer.jsx
+++ projetfinal/src/components/Footer.jsx
@@ -1,30 +1,63 @@
 import React from "react";
+import ButtonComponent from "./ButtonComponent";
+import FooterLink from "./FooterLink";
+import "./Footer.css";
+import "./Button.css";
 
 function Footer() {
-    return (
-      <div className="footer-container">
-        <div className="card bg-dark text-white">
-        
-        <div className="card-body">
-          <h3 className="card-title">Venez nous <img src="assets/heart.png" style={{width:30}} alt="logo amour" /> !</h3>
-          <h5 className="card-subtitle">Ici on cherche seulement de l'amour</h5>
-          <p className="card-text">Aidez nos fidèles compagnons <img src="assets/chat50.png" alt="logo chat" /> <img src="assets/chien50.png" alt="logo chien" />à retrouver une famille aimante</p>
-        </div>
+	return (
+		<div className="footer-container">
+			<div className="footer-text-container">
+				<h3 className="title-footer">
+					Venez nous
+					<img
+						src="assets/heart.png"
+						style={{ width: 30 }}
+						alt="logo amour"
+					/>{" "}
+					!
+				</h3>
 
-        <div className="button-container">
-          <a href="#" className="button">Nous Contacter</a>
-        </div>
-        
-        <div className="footer-links">
-          <a href="https://www.facebook.com/AdoptionChatsNantes/?locale=fr_FR"><img src="assets/fb50.png" alt="logo Facebook" /></a>
-          <a href="https://www.instagram.com/henrythecoloradodog/"><img src="assets/insta50.png" alt="logo Instagram" /></a>
-          <a href="https://twitter.com/chats_chiens?lang=fr"><img src="assets/twi50.png" alt="logo Twitter" /></a>
-        </div>
+				<h5 className="subtitle-footer">Ici on cherche seulement de l'amour</h5>
 
-      </div>
-    </div>
+				<p className="text-footer">
+					Aidez nos fidèles compagnons{" "}
+					<img src="assets/chat50.png" alt="logo chat" />{" "}
+					<img src="assets/chien50.png" alt="logo chien" />à retrouver une
+					famille aimante
+				</p>
+			</div>
 
-    );
-  }
-  
-  export default Footer;
+			<div className="container-link">
+				<div className="links">
+					<FooterLink
+						lien={"https://www.facebook.com/AdoptionChatsNantes/?locale=fr_FR"}
+						img={<img src="assets/fb50.png" alt="logo Facebook" />}
+						handleOnClick={() => {}}
+					></FooterLink>
+
+					<FooterLink
+						lien={"https://www.instagram.com/henrythecoloradodog/"}
+						img={<img src="assets/insta50.png" alt="logo Instagram" />}
+						handleOnClick={() => {}}
+					></FooterLink>
+
+					<FooterLink
+						lien={"https://twitter.com/chats_chiens?lang=fr"}
+						img={<img src="assets/twi50.png" alt="logo Twitter" />}
+						handleOnClick={() => {}}
+					></FooterLink>
+				</div>
+				<div className="button-place">
+					<ButtonComponent
+						lien="/"
+						text="Nous Contacter"
+						handleOnClick={() => {}}
+					></ButtonComponent>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default Footer;
