5ea9c8209ab5a572d528ef1f8e41df006881d17d
Anton-mnc <antoine.mgy.mnc@gmail.com>
2023-05-19 12:07:14 +0200

    Description page v2 dscription placed with the details cards

--- projetfinal/src/components/DescriptionComponent.tsx
+++ projetfinal/src/components/DescriptionComponent.tsx
@@ -1,35 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import "./PageAccueil.css";
 import "./DescriptionComponent.css";
 import "./Button.css";
-import { useParams } from "react-router-dom";
-import { Animal } from "./Animal";
 import ButtonComponent from "./ButtonComponent";
+import animal from "./AnimalData";
 
 function DescriptionComponent() {
-	const { id } = useParams();
-	// const [animal, setAnimal] = useState<Animal>();
-
-	//     fetch(http://localhost:8080/site/rfcompagnon/${id})
-	//       .then((res) => res.json())
-	//       .then((data) => setAnimal(data));
-	//   }, []);
-
-	const animal = new Animal();
-	animal.id = 1;
-	animal.nom = "Felix";
-	animal.race = "Berger Australien";
-	animal.espece = "chien";
-	animal.age = 3;
-	animal.poids = 15;
-	animal.sexe = "Femelle";
-	animal.endroit = "Rennes";
-	animal.urlImage = "chien2.jpg";
-	animal.vaccin = "Oui";
-	animal.sterilise = "Non";
-	animal.prix = 150;
-	animal.description =
-		"Félix est un chat charmant et joueur à la recherche d'une maison pour la vie. Avec ses grands yeux verts et son pelage doux et soyeux, Félix est une véritable beauté. Ce félin élégant a environ deux ans et déborde d'énergie et de curiosité.";
 	return (
 		<div className="description-card-container">
 			<div className="description-container">
--- projetfinal/src/components/PageVueCompagnon.tsx
+++ projetfinal/src/components/PageVueCompagnon.tsx
@@ -1,16 +1,43 @@
 import React, { useEffect, useState } from "react";
+import { Animal } from "./Animal";
+import animal from "./AnimalData";
 import DescriptionComponent from "./DescriptionComponent";
 import "./PageAccueil.css";
 import "./PageVueCompagnon.css";
 
 function PageVueCompagnon() {
+	// const { id } = useParams();
+	// const [animal, setAnimal] = useState<Animal>();
+
+	//     fetch(http://localhost:8080/site/rfcompagnon/${id})
+	//       .then((res) => res.json())
+	//       .then((data) => setAnimal(data));
+	//   }, []);
+
 	return (
-		<div>
-			<div className="container-general-page">
-				<DescriptionComponent></DescriptionComponent>
+		<div className="position-item">
+			<div className="position-container-blank">
+				<div className="container-general-page">
+					<div className="details-container-bottom">
+						<div className="details-container">
+							<div className="object-title-separator">
+								<h2 className="details-title-description">Description</h2>
+								<hr className="separator" />
+							</div>
+							<div className="text-details-improvement">
+								{animal.description}
+							</div>
+						</div>
+					</div>
+					<DescriptionComponent></DescriptionComponent>
+				</div>
 			</div>
+			<div className="position-details"></div>
 		</div>
 	);
 }
 
 export default PageVueCompagnon;
+function useParams(): { id: any } {
+	throw new Error("Function not implemented.");
+}
