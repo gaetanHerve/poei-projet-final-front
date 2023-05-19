f9b96b15cbff74411e68caa6c96e38605299d860
Laure Le Nouail <laure.lenouail@gmail.com>
2023-05-19 15:00:07 +0200

    finition page compte

--- projetfinal/src/components/Compte.tsx
+++ projetfinal/src/components/Compte.tsx
@@ -7,23 +7,11 @@ import { useNavigate } from "react-router-dom";
 
 function Compte() {
   const navigate = useNavigate();
-  const [personne, setPersonne] = useState<Personne>({
-    prenom: "Jean",
-    nom: "Dupond",
-    id: 2,
-    login: "test",
-    password: "mdp1",
-    complement: {
-      adresse: "rue du louvre",
-      telephone: "0600000001",
-      informations: "RAS",
-    },
-    admin: false,
-  });
+  const [personne, setPersonne] = useState<Personne>();
   const [person, setPerson] = useState({});
+
   const [deconnected, setDeconnected] = useState(false);
   let deconnectionAsked = false;
-  setPerson({ ...person, id: personne.id });
 
   useEffect(() => {
     if (sessionStorage.getItem("utilisateur")) {
@@ -33,10 +21,17 @@ function Compte() {
     }
   }, []);
 
+  // useEffect(() => {
+  //   if (personne != undefined) {
+  //     setPerson({ ...person, ...personne });
+  //     personne2 = personne;
+  //   }
+  // }, [personne]);
+
   const requestOptions = {
     method: "PUT",
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(person),
+    body: JSON.stringify(personne),
   };
 
   const handleSubmit = (event) => {
@@ -44,7 +39,8 @@ function Compte() {
     fetch("http://localhost:8080/patoune-moi/personnes", requestOptions).then(
       (response) => {
         if (response.ok) {
-          sessionStorage.setItem("utilisateur", JSON.stringify(person));
+          sessionStorage.setItem("utilisateur", JSON.stringify(personne));
+          alert(`Votre compte a été modifié avec succès !`);
         }
       }
     );
@@ -52,81 +48,92 @@ function Compte() {
   return (
     <div className="compte-container">
       <h1 className="compte-titre">Mon espace</h1>
-      <form onSubmit={handleSubmit} className="compte-container-input">
-        <div className="compte-container-name">
-          <div className="compte-container-label-input">
-            <label className="compte-label">Prénom</label>
-            <input
-              type="text"
-              className="compte-input"
-              onChange={(e) =>
-                setPerson({ ...person, ["prenom"]: e.target.value })
-              }
-              placeholder={personne && personne.prenom}
-            />
+      {personne && person && (
+        <form onSubmit={handleSubmit} className="compte-container-input">
+          <div className="compte-container-name">
+            <div className="compte-container-label-input">
+              <label className="compte-label">Prénom</label>
+              <input
+                type="text"
+                className="compte-input"
+                onChange={(e) =>
+                  setPersonne({ ...personne, ["prenom"]: e.target.value })
+                }
+                value={personne && personne.prenom}
+              />
+            </div>
+            <div className="compte-container-label-input">
+              <label className="compte-label">Nom</label>
+              <input
+                type="text"
+                className="compte-input"
+                onChange={(e) =>
+                  setPersonne({ ...personne, ["nom"]: e.target.value })
+                }
+                value={personne && personne.nom}
+              />
+            </div>
           </div>
-          <div className="compte-container-label-input">
-            <label className="compte-label">Nom</label>
-            <input
-              type="text"
-              className="compte-input"
-              placeholder={personne && personne.nom}
-              onChange={(e) =>
-                setPerson({ ...person, ["nom"]: e.target.value })
-              }
-            />
+          <div className="compte-container-email">
+            <div className="compte-container-label-input">
+              <label className="compte-label">Login</label>
+              <input
+                type="text"
+                className="compte-input"
+                value={personne && personne.login}
+              />
+            </div>
+            <div className="compte-container-label-input">
+              <label>Mot de passe</label>
+              <input
+                type="password"
+                className="compte-input"
+                value={personne && personne.password}
+                onChange={(e) =>
+                  setPersonne({ ...personne, ["password"]: e.target.value })
+                }
+              />
+            </div>
           </div>
-        </div>
-        <div className="compte-container-email">
-          <div className="compte-container-label-input">
-            <label className="compte-label">Login</label>
-            <input
-              type="text"
-              className="compte-input"
-              value={personne && personne.login}
-            />
+          <div className="compte-container-complement">
+            <div className="compte-container-label-input">
+              <label className="compte-label">Téléphone</label>
+              <input
+                type="tel"
+                className="compte-input"
+                value={personne.complement && personne.complement.telephone}
+                onChange={(e) =>
+                  setPersonne({
+                    ...personne,
+                    complement: {
+                      ...personne.complement,
+                      telephone: e.target.value,
+                    },
+                  })
+                }
+              />
+            </div>
+            <div className="compte-container-label-input">
+              <label className="compte-label">Adresse</label>
+              <input
+                type="text"
+                className="compte-input"
+                value={personne.complement && personne.complement.adresse}
+                onChange={(e) =>
+                  setPersonne({
+                    ...personne,
+                    complement: {
+                      ...personne.complement,
+                      adresse: e.target.value,
+                    },
+                  })
+                }
+              />
+            </div>
           </div>
-          <div className="compte-container-label-input">
-            <label>Mot de passe</label>
-            <input
-              type="password"
-              className="compte-input"
-              placeholder={personne && personne.password}
-              onChange={(e) =>
-                setPerson({ ...person, ["password"]: e.target.value })
-              }
-            />
-          </div>
-        </div>
-        <div className="compte-container-complement">
-          <div className="compte-container-label-input">
-            <label className="compte-label">Téléphone</label>
-            <input
-              type="tel"
-              className="compte-input"
-              placeholder={personne && personne.complement.telephone}
-              onChange={(e) =>
-                setPerson({
-                  ...person,
-                  ["complement.telephone"]: e.target.value,
-                })
-              }
-            />
-          </div>
-          <div className="compte-container-label-input">
-            <label className="compte-label">Adresse</label>
-            <input
-              type="text"
-              className="compte-input"
-              placeholder={personne && personne.complement.adresse}
-              onChange={(e) =>
-                setPerson({ ...person, ["complement.adresse"]: e.target.value })
-              }
-            />
-          </div>
-        </div>
-        <input type="submit" className="compte-input-submit" />
-      </form>
+          <input type="submit" className="compte-input-submit" />
+        </form>
+      )}
     </div>
   );
 }
