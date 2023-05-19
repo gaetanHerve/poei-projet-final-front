a77000a1cae2e112e547f5d949aaf19d5ab2d6b4
Laure Le Nouail <laure.lenouail@gmail.com>
2023-05-19 11:41:08 +0200

    page compte

--- projetfinal/src/components/Compte.tsx
+++ projetfinal/src/components/Compte.tsx
@@ -3,66 +3,98 @@ import Personne from "../models/Personne";
 import Complement from "../models/ComplementPersonne";
 import "./Compte.css";
 import ButtonComponent from "./ButtonComponent";
+import { useNavigate } from "react-router-dom";
 
 function Compte() {
+  const navigate = useNavigate();
+  const [personne, setPersonne] = useState<Personne>({
+    prenom: "Jean",
+    nom: "Dupond",
+    id: 2,
+    login: "test",
+    password: "mdp1",
+    complement: {
+      adresse: "rue du louvre",
+      telephone: "0600000001",
+      informations: "RAS",
+    },
+    admin: false,
+  });
   const [person, setPerson] = useState({});
-  //   const [isformsubmit, setIsFormSubmit] = useState(false);
-  // useEffect(() => {
+  const [deconnected, setDeconnected] = useState(false);
+  let deconnectionAsked = false;
+  setPerson({ ...person, id: personne.id });
 
-  //     fetch(`http://localhost:8080/patoune-moi/personnes/${id}`).then((res) => res.json()).then(data => setPerson(data));
-  //     setIsFormSubmit(false);
-  // }, [isformsubmit == true])
+  useEffect(() => {
+    if (sessionStorage.getItem("utilisateur")) {
+      setPersonne({
+        ...JSON.parse(sessionStorage.getItem("utilisateur") ?? ""),
+      });
+    }
+  }, []);
 
-  //   const requestOptions = {
-  //     method: "PUT",
-  //     headers: { "Content-Type": "application/json" },
-  //     body: JSON.stringify(person),
-  //   };
+  const requestOptions = {
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(person),
+  };
 
-  //   const handleSubmit = (event) => {
-  //     event.preventDefault();
-  //     fetch("http://localhost:8080/patoune-moi/personnes", requestOptions);
-  //     setIsFormSubmit(true);
-
-  const person1 = new Personne();
-  person1.nom = "Le Nouail";
-  person1.prenom = "Laure";
-  person1.id = 12;
-  person1.password = "test1";
-  const comp = new Complement();
-  comp.adresse = "9 rue de la mama";
-  comp.telephone = "0606060606";
-  person1.complement = comp;
-  //   };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    fetch("http://localhost:8080/patoune-moi/personnes", requestOptions).then(
+      (response) => {
+        if (response.ok) {
+          sessionStorage.setItem("utilisateur", JSON.stringify(person));
+        }
+      }
+    );
+  };
   return (
     <div className="compte-container">
       <h1 className="compte-titre">Mon espace</h1>
-      <form className="compte-container-input">
+      <form onSubmit={handleSubmit} className="compte-container-input">
         <div className="compte-container-name">
           <div className="compte-container-label-input">
             <label className="compte-label">Prénom</label>
             <input
               type="text"
               className="compte-input"
-              placeholder={person1.prenom}
+              onChange={(e) =>
+                setPerson({ ...person, ["prenom"]: e.target.value })
+              }
+              placeholder={personne && personne.prenom}
             />
           </div>
           <div className="compte-container-label-input">
             <label className="compte-label">Nom</label>
-            <input type="text" className="compte-input" value={person1.nom} />
+            <input
+              type="text"
+              className="compte-input"
+              placeholder={personne && personne.nom}
+              onChange={(e) =>
+                setPerson({ ...person, ["nom"]: e.target.value })
+              }
+            />
           </div>
         </div>
         <div className="compte-container-email">
           <div className="compte-container-label-input">
             <label className="compte-label">Login</label>
-            <input type="text" className="compte-input" value={person1.id} />
+            <input
+              type="text"
+              className="compte-input"
+              value={personne && personne.login}
+            />
           </div>
           <div className="compte-container-label-input">
             <label>Mot de passe</label>
             <input
               type="password"
               className="compte-input"
-              value={person1.password}
+              placeholder={personne && personne.password}
+              onChange={(e) =>
+                setPerson({ ...person, ["password"]: e.target.value })
+              }
             />
           </div>
         </div>
@@ -72,7 +104,13 @@ function Compte() {
             <input
               type="tel"
               className="compte-input"
-              value={person1.complement.telephone}
+              placeholder={personne && personne.complement.telephone}
+              onChange={(e) =>
+                setPerson({
+                  ...person,
+                  ["complement.telephone"]: e.target.value,
+                })
+              }
             />
           </div>
           <div className="compte-container-label-input">
@@ -80,15 +118,16 @@ function Compte() {
             <input
               type="text"
               className="compte-input"
-              value={person1.complement.adresse}
+              placeholder={personne && personne.complement.adresse}
+              onChange={(e) =>
+                setPerson({ ...person, ["complement.adresse"]: e.target.value })
+              }
             />
           </div>
         </div>
-        {/* <ButtonComponent></ButtonComponent> */}
+        <input type="submit" className="compte-input-submit" />
       </form>
     </div>
   );
 }
 export default Compte;
-
-function ModifInfo() {}
--- projetfinal/src/models/Personne.ts
+++ projetfinal/src/models/Personne.ts
@@ -1,11 +1,12 @@
 import Complement from "./ComplementPersonne";
 
 export default class Personne {
-    id: number;
-    password: string;
-    nom: string;
-    prenom: string;
-    admin: boolean;
-    complement: Complement;
-    version?: number;
+  id: number;
+  login: string;
+  password: string;
+  nom: string;
+  prenom: string;
+  admin: boolean;
+  complement: Complement;
+  version?: number;
 }
