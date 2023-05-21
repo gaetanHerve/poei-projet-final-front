ff924f680c2c37091e71682ef392da8ca3739f5a
gaetanHerve <gaetan.herve@lilo.org>
2023-05-21 16:08:27 +0200

    split compte and commandes into 2 components

--- projetfinal/src/App.tsx
+++ projetfinal/src/App.tsx
@@ -30,7 +30,6 @@ function App() {
           <Route path="testfooter" element={<TestNavbarFooter />} />
           <Route path="findbyid/:id" element={<PageVueCompagnon />} />
           <Route path="creercompte" element={<PageCreationUtilisateur />} />
-          <Route path="infoscompte" element={<PageinfosCompte />} />
           <Route path="compte" element={<Compte />} />
 		      <Route path="pagearticle" element={<PageArticle />} />
           <Route path="recappanier" element={<RecapPanier />} />
