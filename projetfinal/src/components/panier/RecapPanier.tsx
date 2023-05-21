c4ed4c397c8ceefc99ebdf83433209e72b58d6c6
gaetanHerve <gaetan.herve@lilo.org>
2023-05-21 22:29:50 +0200

    fix date format in commandes

--- projetfinal/src/components/panier/RecapPanier.tsx
+++ projetfinal/src/components/panier/RecapPanier.tsx
@@ -12,7 +12,14 @@ function RecapPanier() {
 
 	const getCurrentDate = () => {
 		let d = new Date();
-		return `${d.getDate()}/${(d.getMonth() + 1)}/${d.getFullYear()} ${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}`;
+		let dateString = ('0' + d.getDate()).slice(-2) + '/'
+             + ('0' + (d.getMonth()+1)).slice(-2) + '/'
+             + d.getFullYear() + ' à '
+			 + ('0' + (d.getHours())).slice(-2) + ':'
+			 + ('0' + (d.getMinutes())).slice(-2) + ':'
+			 + ('0' + (d.getSeconds())).slice(-2);
+		return dateString;
+		// return `${d.getDate()}/${(d.getMonth() + 1)}/${d.getFullYear()} ${d.getHours()}:${d.getMinutes()}:${'0' + (d.getSeconds()).slice(-2)}`;
 	}
 
 	const [animal, setAnimal] = useState<Animal>({  
